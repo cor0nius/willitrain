d7223fe19a0df1dd7318f600a99fcda1e4389612
cor0nius <jacekglowacki94@gmail.com>
2025-08-01 12:01:57 +0200

    mostly completed Geocode function, needs cleanup

--- geocoding.go
+++ geocoding.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 )
@@ -15,16 +17,49 @@ func (cfg *apiConfig) WrapForGeocode(cityName string) (string, error) {
 }
 
 func (cfg *apiConfig) Geocode(cityName string) (Location, error) {
-	reqURL, err := cfg.WrapForGeocode(cityName)
+	reqestURL, err := cfg.WrapForGeocode(cityName)
 	if err != nil {
 		return Location{}, err
 	}
-	resp, err := http.Get(reqURL)
+
+	response, err := http.Get(reqestURL)
 	if err != nil {
 		return Location{}, err
 	}
-	defer resp.Body.Close()
-	return Location{}, nil
+	defer response.Body.Close()
+
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		return Location{}, err
+	}
+
+	var responseJSON Response
+	if err = json.Unmarshal(data, &responseJSON); err != nil {
+		return Location{}, err
+	}
+
+	if len(responseJSON.Results) == 0 {
+		return Location{}, err // no results found
+	}
+
+	result := responseJSON.Results[0]
+
+	var location Location
+	location.Latitude = result.Geometry.Location.Latitude
+	location.Longitude = result.Geometry.Location.Longitude
+
+	for _, component := range result.AddressComponents {
+		for _, componentType := range component.Types {
+			switch componentType {
+			case "locality":
+				location.CityName = component.LongName
+			case "country":
+				location.CountryCode = component.ShortName
+			}
+		}
+	}
+
+	return location, nil
 }
 
 func (cfg *apiConfig) WrapForReverseGeocode(lat, lng float64) (string, error) {
@@ -32,4 +67,29 @@ func (cfg *apiConfig) WrapForReverseGeocode(lat, lng float64) (string, error) {
 	latlng := fmt.Sprintf("latlng=%v,%v", lat, lng)
 	wrappedURL := fmt.Sprintf("%sjson?%s%s", baseURL, latlng, cfg.gmpKey)
 	return wrappedURL, nil
+}
+
+type Response struct {
+	Results []Result `json:"results"`
+	Status  string   `json:"status"`
+}
+
+type Result struct {
+	AddressComponents []AddressComponent `json:"address_components"`
+	Geometry          Geometry           `json:"geometry"`
+}
+
+type AddressComponent struct {
+	LongName  string   `json:"long_name"`
+	ShortName string   `json:"short_name"`
+	Types     []string `json:"types"`
+}
+
+type Geometry struct {
+	Location LocationData `json:"location"`
+}
+
+type LocationData struct {
+	Latitude  float64 `json:"lat"`
+	Longitude float64 `json:"lng"`
 }
