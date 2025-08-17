f3bf7c983d1ed9386e385952d40c4009e7445389
cor0nius <jacekglowacki94@gmail.com>
2025-08-17 20:50:00 +0200

    small fix in tests

--- weather_wrappers_test.go
+++ weather_wrappers_test.go
@@ -1,49 +1,16 @@
 package main
 
 import (
-	"os"
 	"testing"
-
-	"github.com/joho/godotenv"
 )
 
 func TestURLWrappers(t *testing.T) {
-	err := godotenv.Load()
-	if err != nil {
-		t.Fatalf("Error loading .env file")
-	}
-
-	gmpWeatherURL := os.Getenv("GMP_WEATHER_URL")
-	if gmpWeatherURL == "" {
-		t.Fatal("GMP_WEATHER_URL must be set")
-	}
-
-	gmpKey := os.Getenv("GMP_KEY")
-	if gmpKey == "" {
-		t.Fatal("Missing API Key for Google Maps Platform")
-	}
-
-	owmWeatherURL := os.Getenv("OWM_WEATHER_URL")
-	if owmWeatherURL == "" {
-		t.Fatal("OWM_WEATHER_URL must be set")
-	}
-
-	owmKey := os.Getenv("OWM_KEY")
-	if owmKey == "" {
-		t.Fatal("Missing API Key for OpenWeatherMap")
-	}
-
-	ometeoWeatherURL := os.Getenv("OMETEO_WEATHER_URL")
-	if ometeoWeatherURL == "" {
-		t.Fatal("OMETEO_WEATHER_URL must be set")
-	}
-
 	cfg := apiConfig{
-		gmpWeatherURL:    gmpWeatherURL,
-		gmpKey:           gmpKey,
-		owmWeatherURL:    owmWeatherURL,
-		owmKey:           owmKey,
-		ometeoWeatherURL: ometeoWeatherURL,
+		gmpWeatherURL:    "https://weather.googleapis.com/v1/",
+		gmpKey:           "gmpKey",
+		owmWeatherURL:    "https://api.openweathermap.org/data/3.0/onecall?",
+		owmKey:           "owmKey",
+		ometeoWeatherURL: "https://api.open-meteo.com/v1/forecast?",
 	}
 
 	location := Location{Latitude: 51.1093, Longitude: 17.0386} // Example coordinates for Wrocław
@@ -57,8 +24,8 @@ func TestURLWrappers(t *testing.T) {
 			name:        "CurrentWeather",
 			wrapperFunc: cfg.WrapForCurrentWeather,
 			expectedURLs: map[string]string{
-				"gmpWrappedURL":    "https://weather.googleapis.com/v1/currentConditions:lookup?key=" + gmpKey + "&location.latitude=51.11&location.longitude=17.04",
-				"owmWrappedURL":    "https://api.openweathermap.org/data/3.0/onecall?lat=51.11&lon=17.04&exclude=minutely,hourly,daily,alerts&units=metric&appid=" + owmKey,
+				"gmpWrappedURL":    "https://weather.googleapis.com/v1/currentConditions:lookup?key=" + cfg.gmpKey + "&location.latitude=51.11&location.longitude=17.04",
+				"owmWrappedURL":    "https://api.openweathermap.org/data/3.0/onecall?lat=51.11&lon=17.04&exclude=minutely,hourly,daily,alerts&units=metric&appid=" + cfg.owmKey,
 				"ometeoWrappedURL": "https://api.open-meteo.com/v1/forecast?latitude=51.11&longitude=17.04&current=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation,weather_code&timezone=auto&timeformat=unixtime",
 			},
 		},
@@ -66,8 +33,8 @@ func TestURLWrappers(t *testing.T) {
 			name:        "DailyForecast",
 			wrapperFunc: cfg.WrapForDailyForecast,
 			expectedURLs: map[string]string{
-				"gmpWrappedURL":    "https://weather.googleapis.com/v1/forecast/days:lookup?key=" + gmpKey + "&location.latitude=51.11&location.longitude=17.04",
-				"owmWrappedURL":    "https://api.openweathermap.org/data/3.0/onecall?lat=51.11&lon=17.04&exclude=current,minutely,hourly,alerts&units=metric&appid=" + owmKey,
+				"gmpWrappedURL":    "https://weather.googleapis.com/v1/forecast/days:lookup?key=" + cfg.gmpKey + "&location.latitude=51.11&location.longitude=17.04",
+				"owmWrappedURL":    "https://api.openweathermap.org/data/3.0/onecall?lat=51.11&lon=17.04&exclude=current,minutely,hourly,alerts&units=metric&appid=" + cfg.owmKey,
 				"ometeoWrappedURL": "https://api.open-meteo.com/v1/forecast?latitude=51.11&longitude=17.04&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_probability_max,wind_speed_10m_max,weather_code,relative_humidity_2m_max&timezone=auto&timeformat=unixtime",
 			},
 		},
@@ -75,8 +42,8 @@ func TestURLWrappers(t *testing.T) {
 			name:        "HourlyForecast",
 			wrapperFunc: cfg.WrapForHourlyForecast,
 			expectedURLs: map[string]string{
-				"gmpWrappedURL":    "https://weather.googleapis.com/v1/forecast/hours:lookup?key=" + gmpKey + "&location.latitude=51.11&location.longitude=17.04",
-				"owmWrappedURL":    "https://api.openweathermap.org/data/3.0/onecall?lat=51.11&lon=17.04&exclude=current,minutely,daily,alerts&units=metric&appid=" + owmKey,
+				"gmpWrappedURL":    "https://weather.googleapis.com/v1/forecast/hours:lookup?key=" + cfg.gmpKey + "&location.latitude=51.11&location.longitude=17.04",
+				"owmWrappedURL":    "https://api.openweathermap.org/data/3.0/onecall?lat=51.11&lon=17.04&exclude=current,minutely,daily,alerts&units=metric&appid=" + cfg.owmKey,
 				"ometeoWrappedURL": "https://api.open-meteo.com/v1/forecast?latitude=51.11&longitude=17.04&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation,precipitation_probability,weather_code&forecast_days=2&timezone=auto&timeformat=unixtime",
 			},
 		},
